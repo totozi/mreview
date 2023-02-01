d7fa6122d460486531ddeb605454e8f72b695353
skgydnjs@gmail.com <skgydnjs@gmail.com>
2023-02-01 23:18:51 +0900

    ch7

--- src/main/java/org/zerock/mreview/repository/MovieRepository.java
+++ src/main/java/org/zerock/mreview/repository/MovieRepository.java
@@ -6,13 +6,29 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.zerock.mreview.entity.Movie;
 
+import java.util.List;
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     @Query("SELECT m\n" +
+            "    , mi\n" +
             "    , avg(coalesce(r.grade,0))\n" +
             "    , count(distinct r) \n" +
             " FROM Movie m\n" +
+            " LEFT OUTER JOIN MovieImage mi on mi.movie = m \n" +
             " LEFT OUTER JOIN Review r on r.movie = m \n" +
             "GROUP BY m")
     Page<Object[]> getListPage(Pageable pageable);
+
+
+    @Query("SELECT  m  \n" +
+            "     , mi \n" +
+            "     , avg(coalesce(r.grade,0)) \n" +
+            "     , count(distinct(r))      \n" +
+            "  FROM Movie m \n" +
+            "  LEFT OUTER JOIN MovieImage mi on mi.movie = m \n" +
+            "  LEFT OUTER JOIN Review r on r.movie = m \n" +
+            " WHERE m.mno = :mno \n" +
+            " GROUP BY mi")
+    List<Object[]> getMovieWithAll(Long mno); // 특정 영화 조회
 }
--- src/test/java/org/zerock/mreview/repository/MovieRepositoryTests.java
+++ src/test/java/org/zerock/mreview/repository/MovieRepositoryTests.java
@@ -12,6 +12,7 @@ import org.zerock.mreview.entity.Movie;
 import org.zerock.mreview.entity.MovieImage;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.IntStream;
 
@@ -60,6 +61,18 @@ public class MovieRepositoryTests {
 
         for(Object[] objects : result.getContent()) {
             System.out.println(Arrays.toString(objects));
+        }
+    }
+
+    @Test
+    public void testGetMovieWithAll() {
+
+        List<Object[]> result = movieRepository.getMovieWithAll(90L);
+
+        System.out.println(result);
+
+        for(Object[] arr : result) {
+            System.out.println(Arrays.toString(arr));
         }
     }
 }
