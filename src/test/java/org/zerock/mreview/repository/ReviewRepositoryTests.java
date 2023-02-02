10fc7360e44716c851018919e8bb76b2ada1be1f
skgydnjs@gmail.com <skgydnjs@gmail.com>
2023-02-03 00:01:42 +0900

    ch7

--- src/test/java/org/zerock/mreview/repository/ReviewRepositoryTests.java
+++ src/test/java/org/zerock/mreview/repository/ReviewRepositoryTests.java
@@ -3,14 +3,20 @@ package org.zerock.mreview.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.mreview.entity.Member;
 import org.zerock.mreview.entity.Movie;
 import org.zerock.mreview.entity.Review;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootTest
 public class ReviewRepositoryTests {
+
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Autowired
     private ReviewRepository reviewRepository;
@@ -38,5 +44,36 @@ public class ReviewRepositoryTests {
             reviewRepository.save(movieReview);
         });
     }
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDeleteMember() {
+
+        Long mid = 2L; // Member의 mid
+
+        Member member = Member.builder().mid(mid).build();
+
+        reviewRepository.deleteByMember(member); // FK가 있는 테이블에서 먼저 삭제
+        memberRepository.deleteById(mid);
+    }
+
+    @Test
+    public void testGetMovieReviews() {
+
+        Movie movie = Movie.builder().mno(92L).build();
+
+        List<Review> result = reviewRepository.findByMovie(movie);
+
+        result.forEach(movieReview -> {
+            System.out.print(movieReview.getReviewnum());
+            System.out.print("\t" + movieReview.getGrade());
+            System.out.print("\t" + movieReview.getText());
+            System.out.print("\t" + movieReview.getMember().getEmail());
+            System.out.println("----------------------------------");
+        });
+    }
+
+
 
 }
